405eb1e110aab142d999ddc826b6d8df3144fb2c
Olga Morozova <Morozova_Olga_22@mail.ru>
2022-01-22 20:03:56 +0300

    Menu removed added

--- app/src/main/java/ru/netology/nmedia/activity/RegistrationFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/RegistrationFragment.kt
@@ -1,9 +1,7 @@
 package ru.netology.nmedia.activity
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -20,6 +18,15 @@ class RegistrationFragment : Fragment() {
     )
 
     private var fragmentBinding: FragmentRegistrationBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.removeGroup(R.id.unauthenticated)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
